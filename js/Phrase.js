15a777d92e509a9ea459e6c6d10c6962b003408c
Justice Trent <justice.trent@verizon.com>
2019-07-12 10:09:58 -0400

    Unit 4 project step 4
    Added Phrases to Game class

--- js/Phrase.js
+++ js/Phrase.js
@@ -5,6 +5,7 @@
 class Phrase {
     constructor(phrase) {
         this.phrase= phrase.toLowerCase();
+        
     }
     addPhraseToDisplay() {
 
--- js/app.js
+++ js/app.js
@@ -1,2 +1,2 @@
 const phrase = new Phrase('Life is like a box of chocolates');
-console.log(`Phrase - phrase: ${tempPhrase.phrase}`);
+console.log(`Phrase - phrase: ${phrase.phrase}`);
